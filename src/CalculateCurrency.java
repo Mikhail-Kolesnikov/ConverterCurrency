38950580b9247e7fb9d2efc5f2801a704e3a458b
mikhailkolesnikov <mikhail.kolesnikov@outlook.de>
2024-07-09 19:42:42 +0200

    add Calculate and currency

--- src/CalculateCurrency.java
+++ src/CalculateCurrency.java
@@ -1,2 +1,22 @@
 public interface CalculateCurrency {
+
+    private  double calculate(double currency){
+        int count;
+        if (currency == 1) {
+            double result =  * ;
+            System.out.println(result);
+        }
+        if (currency == 2) {
+            double result = rubl * amount;
+            System.out.println(result);
+        }
+        if (currency == 3) {
+            double result = rubletojpy * amount;
+        }
+
+
+        return
+    }
+
+
 }
--- src/Currency.java
+++ src/Currency.java
@@ -1,3 +1,72 @@
+import java.util.Objects;
+
 public class Currency implements CalculateCurrency {
 
+
+    private double dollar;
+     private double euro;
+     private double uaDollar;
+     private double amount;
+
+    public Currency(double dollar, double euro, double uaDollar, double amount) {
+        this.dollar = dollar;
+        this.euro = euro;
+        this.uaDollar = uaDollar;
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Currency{" +
+                "dollar=" + dollar +
+                ", euro=" + euro +
+                ", uaDollar=" + uaDollar +
+                ", amount=" + amount +
+                '}';
+    }
+
+    public double getDollar() {
+        return dollar;
+    }
+
+    public void setDollar(double dollar) {
+        this.dollar = dollar;
+    }
+
+    public double getEuro() {
+        return euro;
+    }
+
+    public void setEuro(double euro) {
+        this.euro = euro;
+    }
+
+    public double getUaDollar() {
+        return uaDollar;
+    }
+
+    public void setUaDollar(double uaDollar) {
+        this.uaDollar = uaDollar;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Currency currency = (Currency) o;
+        return Double.compare(dollar, currency.dollar) == 0 && Double.compare(euro, currency.euro) == 0 && Double.compare(uaDollar, currency.uaDollar) == 0 && Double.compare(amount, currency.amount) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dollar, euro, uaDollar, amount);
+    }
 }
